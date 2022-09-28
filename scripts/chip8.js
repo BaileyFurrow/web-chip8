399cb3f3967185daf07c144c889ae53140a1d258
BaileyFurrow <66973680+BaileyFurrow@users.noreply.github.com>
2022-09-28 17:52:34 -0400

    added ui for controls

--- scripts/chip8.js
+++ scripts/chip8.js
@@ -1,18 +1,29 @@
+/*
+    CHIP-8 Emulator
+    Bailey Furrow
+
+    TODO:
+    [] Audio control (mute, change frequency, change wave type (sine, triangle), etc)
+    [] Ability to change render scale and emulator speed from the UI
+    [] Pause and unpause
+    [] Ability to save and load a save
+    [] ROM selection
+*/
 import Renderer from './renderer.js';
 import Keyboard from './keyboard.js';
 import Speaker from './speaker.js';
 import CPU from './cpu.js';
-
-const renderer = new Renderer(30);
-const keyboard = new Keyboard();
-const speaker = new Speaker();
-const cpu = new CPU(renderer, keyboard, speaker);
 
 let loop;
 
 let fps = 60, fpsInterval, startTime, now, then, elapsed;
 
 function init() {
+    const renderer = new Renderer(30);
+    const keyboard = new Keyboard();
+    const speaker = new Speaker();
+    const cpu = new CPU(renderer, keyboard, speaker);
+
     fpsInterval = 1000 / fps;
     then = Date.now();
     startTime = then;
@@ -21,16 +32,17 @@ function init() {
     cpu.loadRom('BLITZ');
 
     loop = requestAnimationFrame(step);
-}
+    
+    function step() {
+        now = Date.now();
+        elapsed = now-then;
+        if (elapsed > fpsInterval) {
+            cpu.cycle();
+        }
 
-function step() {
-    now = Date.now();
-    elapsed = now-then;
-    if (elapsed > fpsInterval) {
-        cpu.cycle();
+        loop = requestAnimationFrame(step);
     }
-
-    loop = requestAnimationFrame(step);
 }
 
-init();
+
+document.querySelector('.start-button').addEventListener("click", init);
